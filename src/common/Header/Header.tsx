a26cad21659b1261a17cdd0fec4adc11a70bc6e2
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-12-05 15:13:32 +0100

    style: add link active state styles

--- src/common/Header/Header.tsx
+++ src/common/Header/Header.tsx
@@ -2,50 +2,110 @@ import { NavLink } from 'react-router-dom';
 import './Header.scss';
 
 const Header = () => {
+  const activeStyle = {
+    mercure: { color: '#fff', borderColor: '#419ebb' },
+    venus: { color: '#fff', borderColor: '#eda249' },
+    earth: { color: '#fff', borderColor: '#6f2ed6' },
+    mars: { color: '#fff', borderColor: '#cd5120' },
+    jupiter: { color: '#fff', borderColor: '#d83a34' },
+    saturn: { color: '#fff', borderColor: '#d14c32' },
+    uranus: { color: '#fff', borderColor: '#1ec2a4' },
+    neptune: { color: '#fff', borderColor: '#2d68f0' },
+  };
   return (
     <header className="header">
-      <h1 className="header__logo">The Planets</h1>
+      <h1 className="header__logo">
+        <NavLink to="/">The Planets</NavLink>
+      </h1>
 
       <nav className="header__navigation">
         <button className="header__hamburger">Menu</button>
         <ul className="header__menu">
           <li>
-            <NavLink className="header__link" to="/mercury">
+            <NavLink
+              style={({ isActive }) =>
+                isActive ? activeStyle.mercure : undefined
+              }
+              className="header__link"
+              to="/mercury"
+            >
               Mercury
             </NavLink>
           </li>
           <li>
-            <NavLink className="header__link" to="/venus">
+            <NavLink
+              style={({ isActive }) =>
+                isActive ? activeStyle.venus : undefined
+              }
+              className="header__link"
+              to="/venus"
+            >
               Venus
             </NavLink>
           </li>
           <li>
-            <NavLink className="header__link" to="/earth">
+            <NavLink
+              style={({ isActive }) =>
+                isActive ? activeStyle.earth : undefined
+              }
+              className="header__link"
+              to="/earth"
+            >
               Earth
             </NavLink>
           </li>
           <li>
-            <NavLink className="header__link" to="/mars">
+            <NavLink
+              style={({ isActive }) =>
+                isActive ? activeStyle.mars : undefined
+              }
+              className="header__link"
+              to="/mars"
+            >
               Mars
             </NavLink>
           </li>
           <li>
-            <NavLink className="header__link" to="/jupiter">
+            <NavLink
+              style={({ isActive }) =>
+                isActive ? activeStyle.jupiter : undefined
+              }
+              className="header__link"
+              to="/jupiter"
+            >
               Jupiter
             </NavLink>
           </li>
           <li>
-            <NavLink className="header__link" to="/saturn">
+            <NavLink
+              style={({ isActive }) =>
+                isActive ? activeStyle.saturn : undefined
+              }
+              className="header__link"
+              to="/saturn"
+            >
               Saturn
             </NavLink>
           </li>
           <li>
-            <NavLink className="header__link" to="/uranus">
+            <NavLink
+              style={({ isActive }) =>
+                isActive ? activeStyle.uranus : undefined
+              }
+              className="header__link"
+              to="/uranus"
+            >
               Uranus
             </NavLink>
           </li>
           <li>
-            <NavLink className="header__link" to="/neptune">
+            <NavLink
+              style={({ isActive }) =>
+                isActive ? activeStyle.neptune : undefined
+              }
+              className="header__link"
+              to="/neptune"
+            >
               Neptune
             </NavLink>
           </li>
